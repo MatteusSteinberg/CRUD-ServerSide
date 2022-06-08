c6a4a90e58a493c173b86087d62d294bb9838011
MatteusSteinberg <53737794+MatteusSteinberg@users.noreply.github.com>
2022-06-08 11:14:21 +0200

    createUser

--- components/createModal.js
+++ components/createModal.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useRef } from "react"
+import axios from "axios"
 
 export default function CreateModal({ isOpen, setIsOpen }) {
-
+    const form = useRef({})
+    const buttonOnClick = async (ev) => {
+        ev.preventDefault()
+        const formValues = {
+            firstName: form.current.firstName.value,
+            lastName: form.current.lastName.value,
+            username: form.current.username.value,
+            role: form.current.role.value,
+        }
+        const response = await axios.post("/api/createUser", formValues)
+        if (response.data.success == false) {
+            setErrorMessage(response.data.error)
+            setIsError(true)
+            setLoading(false)
+        }
+    }
     return (
-        <div className={isOpen == true ? 'modal createModal is-open' : 'modal createModal'}>
+        <div className={isOpen == true ? "modal createModal is-open" : "modal createModal"}>
             <div className="modal-inner">
                 <div className="modal-container">
                     <div className="modal-header">
@@ -15,19 +31,19 @@ export default function CreateModal({ isOpen, setIsOpen }) {
                             <form id="createUser">
                                 <div className="form-first-name">
                                     <label htmlFor="first-name">First name</label>
-                                    <input type="text" name="first-name" className="input-field" />
+                                    <input type="text" name="first-name" className="input-field" ref={(input) => (form.current.firstName = input)} />
                                 </div>
                                 <div className="form-last-name">
                                     <label htmlFor="last-name">Last name</label>
-                                    <input type="text" name="last-name" className="input-field" />
+                                    <input type="text" name="last-name" className="input-field" ref={(input) => (form.current.lastName = input)} />
                                 </div>
                                 <div className="form-username">
                                     <label htmlFor="username">Username</label>
-                                    <input type="text" name="username" className="input-field" />
+                                    <input type="text" name="username" className="input-field" ref={(input) => (form.current.username = input)} />
                                 </div>
                                 <div className="form-role">
                                     <label htmlFor="role">Role</label>
-                                    <select name="role" className="input-field">
+                                    <select name="role" className="input-field" ref={(input) => (form.current.role = input)}>
                                         <option defaultValue>Select role...</option>
                                         <option>User</option>
                                         <option>Admin</option>
@@ -39,7 +55,7 @@ export default function CreateModal({ isOpen, setIsOpen }) {
                         </div>
                     </div>
                     <div className="modal-footer">
-                        <button className="btn btn-modal btn-create">
+                        <button className="btn btn-modal btn-create" onClick={buttonOnClick}>
                             Create
                         </button>
                         <button className="btn btn-modal btn-cancel" onClick={(e) => setIsOpen(false)}>
@@ -48,6 +64,6 @@ export default function CreateModal({ isOpen, setIsOpen }) {
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
---
+++ pages/api/createUser.js
@@ -0,0 +1,27 @@
+import nc from "next-connect"
+import dbConnect from "../../lib/dbConnect"
+import User from "../../models/User"
+
+const handler = nc({
+    onError: (err, req, res, next) => {
+        console.error(err.stack)
+        res.status(500).end("Something broke!")
+    },
+    onNoMatch: (req, res) => {
+        res.status(404).end("Page is not found")
+    },
+}).post(async (req, res) => {
+    const { firstName, lastName, username, role } = req.body
+    console.log(req.body)
+    await dbConnect()
+
+    const user = new User({
+        firstName,
+        lastName,
+        username,
+        role,
+    })
+    await user.save()
+    res.status(200).json({ success: true })
+})
+export default handler

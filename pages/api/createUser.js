8319aaa8135b05c5d12addbb5d27431f99cc2de1
MatteusSteinberg <53737794+MatteusSteinberg@users.noreply.github.com>
2022-06-09 08:55:11 +0200

    f

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,27 +1,31 @@
-import nc from "next-connect"
-import dbConnect from "../../lib/dbConnect"
-import User from "../../models/User"
+import nc from 'next-connect';
+import dbConnect from '../../lib/dbConnect';
+import User from '../../models/User';
 
 const handler = nc({
-    onError: (err, req, res, next) => {
-        console.error(err.stack)
-        res.status(500).end("Something broke!")
-    },
-    onNoMatch: (req, res) => {
-        res.status(404).end("Page is not found")
-    },
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).end('Something broke!');
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end('Page is not found');
+  }
 }).post(async (req, res) => {
-    const { firstName, lastName, username, role } = req.body
+  const { firstName, lastName, username, role } = req.body;
 
-    await dbConnect()
+  await dbConnect();
 
+  const existingUser = User.findOne({ username: username });
+  if (!existingUser) {
     const user = new User({
-        firstName,
-        lastName,
-        username,
-        role,
-    })
-    await user.save()
-    res.status(200).json({ success: true })
-})
-export default handler
+      firstName,
+      lastName,
+      username,
+      role
+    });
+    await user.save();
+    res.status(200).json({ success: true });
+  }
+  res.status(500).json({ success: false });
+});
+export default handler;

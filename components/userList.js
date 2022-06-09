f53e821ae1bfd7c100acd7a9e47439ddef4c6945
MatteusSteinberg <53737794+MatteusSteinberg@users.noreply.github.com>
2022-06-09 12:17:49 +0200

    Update userList.js

--- components/userList.js
+++ components/userList.js
@@ -30,7 +30,7 @@ export default function UserList() {
       });
       if (res.data.success) {
         const newUser = users.filter((el) => el._id !== id);
-        setUsers(newUser);
+        setUsers(newUser, 'hej');
       }
     } catch (err) {
       console.error(err);
